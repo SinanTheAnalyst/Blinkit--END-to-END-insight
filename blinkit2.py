ad1e25c532da772ce5ef2ff76a73c2b6194ceec2
SinanTheAnalyst <sinanmhd854@gmail.com>
2026-07-27 20:27:49 +0530

    Add files via upload

---
+++ blinkit2.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+df = pd.read_csv("/Users/macairm4/Documents/BlinkIT Grocery Data.csv")
+print(df.head(20))
